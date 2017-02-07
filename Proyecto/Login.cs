47777ed6e021663a7af60a4a4f840827ecb10437
Alexander Olmedo <olmedo_2408@hotmail.com>
2017-02-06 23:35:07 -0500

    Agregar funcion de cerrar al boton cancelar de login

--- Proyecto/Login.cs
+++ Proyecto/Login.cs
@@ -42,5 +42,10 @@ namespace Proyecto
         {
             Application.Exit();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
