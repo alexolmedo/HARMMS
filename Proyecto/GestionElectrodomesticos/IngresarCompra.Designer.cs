6705645bc92499aafa0869a6a92bead1c90ef75d
GD <GD@DESKTOP-8HCJQBV>
2017-02-08 12:46:09 -0500

    Cambio

--- Proyecto/GestionElectrodomesticos/IngresarCompra.Designer.cs
+++ Proyecto/GestionElectrodomesticos/IngresarCompra.Designer.cs
@@ -31,6 +31,10 @@
             this.Agregar = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.cbMesesIngElectV = new System.Windows.Forms.ComboBox();
             this.cbAñosIngElectV = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -45,10 +49,6 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label7 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -102,6 +102,38 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Datos Electrodoméstico Usado";
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(196, 279);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 53;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(78, 282);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(58, 15);
+            this.label8.TabIndex = 52;
+            this.label8.Text = "Contacto:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(196, 172);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 51;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(93, 15);
+            this.label7.TabIndex = 50;
+            this.label7.Text = "Precio de Venta";
+            // 
             // cbMesesIngElectV
             // 
             this.cbMesesIngElectV.FormattingEnabled = true;
@@ -142,7 +174,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(56, 211);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(89, 15);
             this.label1.TabIndex = 47;
             this.label1.Text = "Tiempo de uso";
             // 
@@ -186,7 +218,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(35, 248);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 13);
+            this.label5.Size = new System.Drawing.Size(112, 15);
             this.label5.TabIndex = 41;
             this.label5.Text = "Propietario Anterior";
             // 
@@ -195,7 +227,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(40, 140);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.Size = new System.Drawing.Size(106, 15);
             this.label4.TabIndex = 40;
             this.label4.Text = "Precio de Compra";
             // 
@@ -204,7 +236,7 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(48, 100);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 13);
+            this.label3.Size = new System.Drawing.Size(101, 15);
             this.label3.TabIndex = 39;
             this.label3.Text = "Número de Serie";
             // 
@@ -213,7 +245,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(94, 63);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.Size = new System.Drawing.Size(49, 15);
             this.label2.TabIndex = 38;
             this.label2.Text = "Modelo";
             // 
@@ -222,9 +254,9 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(87, 26);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.Size = new System.Drawing.Size(31, 15);
             this.label6.TabIndex = 37;
-            this.label6.Text = "Nombre";
+            this.label6.Text = "Tipo";
             // 
             // button1
             // 
@@ -237,38 +269,6 @@
             this.button1.Text = "Nuevo";
             this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(196, 172);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 20);
-            this.textBox1.TabIndex = 51;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(53, 175);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(83, 13);
-            this.label7.TabIndex = 50;
-            this.label7.Text = "Precio de Venta";
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(196, 279);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(140, 20);
-            this.textBox2.TabIndex = 53;
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(78, 282);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(53, 13);
-            this.label8.TabIndex = 52;
-            this.label8.Text = "Contacto:";
             // 
             // IngresarCompra
             // 
