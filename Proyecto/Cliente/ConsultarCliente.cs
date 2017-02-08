23d31af3580a42fde1c87829f7fc360f0ccfdecb
Alexander Olmedo <olmedo2408@gmail.com>
2017-02-07 21:42:19 -0500

    Autocompletar búsqueda en modulo consultar cliente

--- Proyecto/Cliente/ConsultarCliente.cs
+++ Proyecto/Cliente/ConsultarCliente.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,68 @@ namespace Proyecto.Cliente
 {
     public partial class ConsultarCliente : Form
     {
+        Conexion conexion = new Conexion();
+       
+        SqlDataAdapter da;
+        DataTable dt;
+
         public ConsultarCliente()
         {
             InitializeComponent();
             this.CenterToScreen();
+            
+            //Llenar los datos para autocompletar la búsqueda por cedula
+            string strquery1 = "Select ci_cliente from cliente";
+            conexion.command = new SqlCommand(strquery1, conexion.connection);
+
+            da = new SqlDataAdapter();
+            //fetching query in the database.
+            da.SelectCommand = conexion.command;
+            //inicializar nueva datatable
+            dt = new DataTable();
+            //refresca las filas segun el rango especificado en el datasource. 
+            da.Fill(dt);
+
+            txtCedula.AutoCompleteCustomSource.Clear();
+            foreach (DataRow r in dt.Rows)
+            {
+                //obtiene todas las filas de una columna
+                var rw = r.Field<string>("ci_cliente");
+
+                //Set the properties of a textbox to make it auto suggest and append.
+                txtCedula.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                txtCedula.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                //adding all rows into the textbox
+                txtCedula.AutoCompleteCustomSource.Add(rw);
+            }
+
+            //Llenar los datos para autocompletar la búsqueda por nombre
+            string strquery2 = "Select nombrecliente from cliente";
+            conexion.command = new SqlCommand(strquery2, conexion.connection);
+
+            da = new SqlDataAdapter();
+            //fetching query in the database.
+            da.SelectCommand = conexion.command;
+            //inicializar nueva datatable
+            dt = new DataTable();
+            //refresca las filas segun el rango especificado en el datasource. 
+            da.Fill(dt);
+
+            txtNombre.AutoCompleteCustomSource.Clear();
+            foreach (DataRow r in dt.Rows)
+            {
+                //obtiene todas las filas de una columna
+                var rw = r.Field<string>("nombrecliente");
+
+                //Set the properties of a textbox to make it auto suggest and append.
+                txtNombre.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                txtNombre.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                //adding all rows into the textbox
+                txtNombre.AutoCompleteCustomSource.Add(rw);
+            }
+
+
+
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
