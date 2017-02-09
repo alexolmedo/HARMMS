f38f0fe19e7d5a567dd57d71959d58581e6208d4
mayliz95 <mayliz.95@hotmail.com>
2017-02-09 07:52:57 -0500

    Modulo Administración

---
+++ Proyecto/Administración/ParametrosFactura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto.Administración
+{
+    public partial class ParametrosFactura : Form
+    {
+        public ParametrosFactura()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCerrarAgrCliente_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
