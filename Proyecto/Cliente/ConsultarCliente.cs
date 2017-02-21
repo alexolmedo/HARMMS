2c85265a3d96d13111d90a3cf831ae43e856e113
mayliz95 <mayliz.95@hotmail.com>
2017-02-20 22:35:06 -0500

    eliminar metodos que no se usaban

--- Proyecto/Cliente/ConsultarCliente.cs
+++ Proyecto/Cliente/ConsultarCliente.cs
@@ -21,56 +21,8 @@ namespace Proyecto.Cliente
         {
             InitializeComponent();
             this.CenterToScreen();
-            
-            //Llenar los datos para autocompletar la búsqueda por cedula
-            string strquery1 = "Select ci_cliente from cliente";
-            conexion.command = new SqlCommand(strquery1, conexion.connection);
-
-            da = new SqlDataAdapter();
-            //fetching query in the database.
-            da.SelectCommand = conexion.command;
-            //inicializar nueva datatable
-            dt = new DataTable();
-            //refresca las filas segun el rango especificado en el datasource. 
-            da.Fill(dt);
-
-            txtCedula.AutoCompleteCustomSource.Clear();
-            foreach (DataRow r in dt.Rows)
-            {
-                //obtiene todas las filas de una columna
-                var rw = r.Field<string>("ci_cliente");
-
-                //Set the properties of a textbox to make it auto suggest and append.
-                txtCedula.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
-                txtCedula.AutoCompleteSource = AutoCompleteSource.CustomSource;
-                //adding all rows into the textbox
-                txtCedula.AutoCompleteCustomSource.Add(rw);
-            }
-
-            //Llenar los datos para autocompletar la búsqueda por nombre
-            string strquery2 = "Select nombrecliente from cliente";
-            conexion.command = new SqlCommand(strquery2, conexion.connection);
-
-            da = new SqlDataAdapter();
-            //fetching query in the database.
-            da.SelectCommand = conexion.command;
-            //inicializar nueva datatable
-            dt = new DataTable();
-            //refresca las filas segun el rango especificado en el datasource. 
-            da.Fill(dt);
-
-            txtNombre.AutoCompleteCustomSource.Clear();
-            foreach (DataRow r in dt.Rows)
-            {
-                //obtiene todas las filas de una columna
-                var rw = r.Field<string>("nombrecliente");
-
-                //Set the properties of a textbox to make it auto suggest and append.
-                txtNombre.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
-                txtNombre.AutoCompleteSource = AutoCompleteSource.CustomSource;
-                //adding all rows into the textbox
-                txtNombre.AutoCompleteCustomSource.Add(rw);
-            }
+            autoCompletarPorCedula();
+            autocompletarPorNombre();
 
         }
 
@@ -177,6 +129,62 @@ namespace Proyecto.Cliente
             else
             {
                 e.Handled = true;
+            }
+        }
+
+        private void autoCompletarPorCedula()
+        {
+            //Llenar los datos para autocompletar la búsqueda por cedula
+            string strquery1 = "Select ci_cliente from cliente";
+            conexion.command = new SqlCommand(strquery1, conexion.connection);
+
+            da = new SqlDataAdapter();
+            //fetching query in the database.
+            da.SelectCommand = conexion.command;
+            //inicializar nueva datatable
+            dt = new DataTable();
+            //refresca las filas segun el rango especificado en el datasource. 
+            da.Fill(dt);
+
+            txtCedula.AutoCompleteCustomSource.Clear();
+            foreach (DataRow r in dt.Rows)
+            {
+                //obtiene todas las filas de una columna
+                var rw = r.Field<string>("ci_cliente");
+
+                //Set the properties of a textbox to make it auto suggest and append.
+                txtCedula.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                txtCedula.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                //adding all rows into the textbox
+                txtCedula.AutoCompleteCustomSource.Add(rw);
+            }
+        }
+
+        private void autocompletarPorNombre(){
+
+            //Llenar los datos para autocompletar la búsqueda por nombre
+            string strquery2 = "Select nombrecliente from cliente";
+            conexion.command = new SqlCommand(strquery2, conexion.connection);
+
+            da = new SqlDataAdapter();
+            //fetching query in the database.
+            da.SelectCommand = conexion.command;
+            //inicializar nueva datatable
+            dt = new DataTable();
+            //refresca las filas segun el rango especificado en el datasource. 
+            da.Fill(dt);
+
+            txtNombre.AutoCompleteCustomSource.Clear();
+            foreach (DataRow r in dt.Rows)
+            {
+                //obtiene todas las filas de una columna
+                var rw = r.Field<string>("nombrecliente");
+
+                //Set the properties of a textbox to make it auto suggest and append.
+                txtNombre.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                txtNombre.AutoCompleteSource = AutoCompleteSource.CustomSource;
+                //adding all rows into the textbox
+                txtNombre.AutoCompleteCustomSource.Add(rw);
             }
         }
     }
